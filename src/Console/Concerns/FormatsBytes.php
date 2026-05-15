e5eae13f7eb6153c322e6cc7f58d84530f4d858f
TanDuy03 <ng.tanduy261203@gmail.com>
2026-05-15 12:50:13 +0700

    feat: add FormatsBytes trait for byte formatting utility

---
+++ src/Console/Concerns/FormatsBytes.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ntanduy\CFD1\Console\Concerns;
+
+trait FormatsBytes
+{
+    protected function formatBytes(int $bytes): string
+    {
+        if ($bytes >= 1073741824) {
+            return round($bytes / 1073741824, 2).' GB';
+        }
+        if ($bytes >= 1048576) {
+            return round($bytes / 1048576, 2).' MB';
+        }
+        if ($bytes >= 1024) {
+            return round($bytes / 1024, 2).' KB';
+        }
+
+        return $bytes.' B';
+    }
+}
